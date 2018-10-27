606a172cd9396dfd6d4085fdfa559120bb6439dd
Valter Silveira <hanzo.junior@gmail.com>
2018-10-27 16:10:43 -0400

    adicionando classe Empresa para teste

---
+++ source/sigcore-api/src/main/java/org/ipdec/sig/model/Empresa.java
@@ -0,0 +1,30 @@
+package org.ipdec.sig.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+
+@Entity
+@Table(name="empresa", schema = "acesso")
+@Data
+public class Empresa {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator="empresa_sequence")
+	@SequenceGenerator(name = "empresa_sequence", sequenceName = "acesso.categoria_codigo_seq", allocationSize = 1)
+	@Column(name="id_empresa")
+	private Long id;
+	
+	@NotNull
+	@Size(min=3, max=50)
+	private String nome;
+}
+
